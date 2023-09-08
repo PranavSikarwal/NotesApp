5ecf6dccc9963845c69628724c6182ad243002e2
NIKUNJ AGARWAL <133116644+nikunj95agarwal@users.noreply.github.com>
2023-09-08 12:21:20 +0530

    Add files via upload
    upload button temporary

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Routes, Route } from "react-router-dom";
 import Heading from "./Heading";
 import Preloader from "./Preloader";
+import Navbar from "./Navbar";
+import Upload from "./Upload"; // Import your Upload component here
+
 function App() {
-    return (
-      <>
-      <Preloader/>
-      <Heading/>
-      
-       </>
-    );
-  }
-  
-  export default App;
-  
+  return (
+    <>
+      <Preloader />
+      <Heading />
+      <Upload/>
+      {/* <Navbar /> */}
+      {/* <Routes> */}
+        {/* <Route path="/Upload" element={<Upload />} /> */}
+        {/* Define other routes here */}
+      {/* </Routes> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,22 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// function Navbar() {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <Link to="/Upload">Upload</Link>
+//         </li>
+//         <li>
+//           <Link to="/Home">HOME</Link>
+//         </li>
+//         <li>
+//           <Link to="/about-us">About Us</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
