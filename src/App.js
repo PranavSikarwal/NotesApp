c0588a7bc3824a188e16e969e1b94f0138bc00ef
NIKUNJ AGARWAL <133116644+nikunj95agarwal@users.noreply.github.com>
2023-09-08 11:40:31 +0530

    Add files via upload
    check for error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { Routes, Route } from "react-router-dom";
 import Heading from "./Heading";
 import Preloader from "./Preloader";
+import Navbar from "./Navbar";
+import Upload from "./Upload"; // Import your Upload component here
+
 function App() {
-    return (
-      <>
-      <Preloader/>
-      <Heading/>
-      
-       </>
-    );
-  }
-  
-  export default App;
-  
+  return (
+    <>
+      <Preloader />
+      <Heading />
+      <Navbar />
+      <Routes>
+        <Route path="/Upload" element={<Upload />} />
+        {/* Define other routes here */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
