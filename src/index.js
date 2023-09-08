095af10e9ff3ac4c3504c3ef4ab3fa9e9fb7f4bd
NIKUNJ AGARWAL <133116644+nikunj95agarwal@users.noreply.github.com>
2023-09-08 12:54:52 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-   
-      <App />
-   
-  </React.StrictMode>
-);
+import App from './App.js';
+
+ReactDOM.render(<>
+<App/>
+</>, document.getElementById("root"));
